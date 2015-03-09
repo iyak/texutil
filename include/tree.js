63ba7c2112ef87b7b682cf431277ee6339672111
hiroshi <miyake@outlook.in>
2015-03-09 21:31:58 +0900

    fixed tree view. support input{} without .tex

--- include/tree.js
+++ include/tree.js
@@ -27,83 +27,77 @@ var definitelyOpen = function(fullpath)
     var objShell = new ActiveXObject("shell.application");
     objShell.ShellExecute(editor, fullpath, "", "open", 4);
 }
-
-var putLinkToFilename = function(fobj)
-{
-    /* construct clickable file tiles.
-    */
-    var fullpath = fobj.path.replace(/\\/g, "\\\\");
-    var link = "<span class=\"treeViewFile\">"
-        + "<span class=\"filename\""
-        + "name=\"" + fullpath + "\">"
-        + fobj.Name
-        + "</span></span>";
-    return(link);
-}
-var showFolderFileList = function(folderspec)
-{
-    /* get file system information here and pass it to other functions
-    */
-    var fso, f, fc, s;
-    fso = new ActiveXObject("Scripting.FileSystemObject");
-    f = fso.GetFolder(folderspec);
-    fc = new Enumerator(f.files);
-    s = "";
-    for (; !fc.atEnd(); fc.moveNext())
-    {
-        s += putLinkToFilename(fc.item());
-        s += "<br>";
-    }
-    return(s);
-}
 var refreshTreeView = function()
 {
+    /* construct tree structure
+     * node.id = file name with extension.
+     * node.path = file directory.
+     * node.children = array of children nodes.
+     */
     var fso = new ActiveXObject("Scripting.FileSystemObject");
-    var dir = fso.GetFolder(".");
-    var file = new Enumerator(dir.files);
+    var workingDir = fso.getFolder(".");
+
+    /* get array of all tex files under the working directory */
+    var fileObjArray = [];
+    var dfsListFiles = function(array, folderObj) {
+        var fileCollector = new Enumerator(folderObj.files);
+        for (; !fileCollector.atEnd(); fileCollector.moveNext()) {
+            var fileObj = fileCollector.item();
+            if (-1 == fileObj.name.search(/\.tex$/)) { /* ignore non-tex files */
+                continue;
+            }
+            array.push(fileObj);
+        }
+        var folderCollector = new Enumerator(folderObj.subFolders);
+        for (; !folderCollector.atEnd(); folderCollector.moveNext()) {
+            dfsListFiles(array, folderCollector.item());
+        }
+        return;
+    }
+    dfsListFiles(fileObjArray, workingDir);
+
+    /* read tex files' dependency to pass it to jstree */
     var nodes = [];
     var isChild = [];
-    var dfs = function(node) {
-        //alert(JSON.stringify(node));
+    var dfsTexDependency = function(fileObj) {
         /* readonly as ascii */
-        var textFile = fso.OpenTextFile(node.id, 1, true, 0);
-        while(!textFile.AtEndOfStream) {
-            /* line by line */
+        var textFile = fso.OpenTextFile(fileObj.path, 1/* readonly */, false/* create? */, 0/* ascii */);
+        var node = {id: fileObj.path, text: fileObj.name, children: [], state: {opened: true}};
+        while(!textFile.atEndOfStream) {
+            /* read line by line */
             var line = textFile.readLine();
             /* match input or include (or includegraphics??) */
             /* TODO?: support comment-out handling */
-            var inputRe = /\\input\{(.+?)\}|\\include\{(.+?)\}/g;
-            var match = inputRe.exec(line);
-            if (null === match) { /* not hit */
+            var child;
+            var inputRegexp = /\\input *\{ *(.+?) *\}/g;
+            var includeRegexp = /\\include *\{ *(.+?) *\}/g;
+            if (undefined != (child = inputRegexp.exec(line)/* [0] is whole matched string */)) {
+                child = child[1];
+                if (-1 == child.search(/\.tex$/)) { /* input{} allows for extension to be missed */
+                    child = child + ".tex";
+                }
+            }
+            else if (undefined != (child = includeRegexp.exec(line))) {
+                child = child[1];
+            }
+            else { /* nothing hit */
                 continue;
             }
-            match.splice(0,1); /* first element is whole matching string so remove it */
-            match = $.grep(match, function(n) {return(n);}); /* erase undefined elements */
-
-            /* create new child node */
-            var child = {id: match[0], text: match[0], children: [], state: {opened: true}};            
-            isChild[match[0]] = true;
-
-            dfs(child);
-            node.children.push(child);
+            childObj = fso.getFile(fileObj.ParentFolder.path + "/" + child);
+            alert(childObj.path);
+            isChild[childObj.path] = true;
+            node.children.push(dfsTexDependency(childObj));
         }
+        return(node);
     }
-    for (; !file.atEnd(); file.moveNext())
-    {
-        /* ignore files without .tex extension */
-        if (-1 == file.item().name.search(/\.tex$/)) {
-            continue;
-        }
-
-        /* create new node of jstree */
-        var node = {id: file.item().name, text: file.item().name, children: [], state: {opened: true}};
-        dfs(node);
-        nodes.push(node);
+    for (var i = 0; i < fileObjArray.length; ++ i) {
+        nodes.push(dfsTexDependency(fileObjArray[i]));
     }
     /* erase children nodes from root */
     nodes = $.grep(nodes, function(node) {return(undefined === isChild[node.id]);});
     
+    /* pass node information to jstree */
     $('#treeViewArea').jstree({core: { data: nodes}}).on("select_node.jstree", function(e, data){
-        openExternally(data.node.text);
+        openExternally(data.node.id);
     });
 }
